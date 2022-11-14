d218978c20a4cf28ac84ce1629e08c64287b60fe
Kirill Salfetnik <oriole001@gmail.com>
2022-11-14 02:49:40 +0100

    Implement basic chat UI

--- frontend/src/components/ScrollableChat.js
+++ frontend/src/components/ScrollableChat.js
@@ -1,20 +1,19 @@
-import { useAuthContext } from '../hooks/useAuthContext';
-
 const isSameSenderMargin = (messages, m, i, userId) => {
     if (
-        i < messages.length - 1 &&
+        i < (messages.length - 1) &&
         messages[i + 1].sender._id === m.sender._id &&
         messages[i].sender._id !== userId
     )
-    return 33;
+    return 35;
     
     else if (
-        (i < messages.length - 1 &&
+        (i < (messages.length - 1) &&
             messages[i + 1].sender._id !== m.sender._id &&
             messages[i].sender._id !== userId) ||
-        (i === messages.length - 1 && messages[i].sender._id !== userId)
+        (i === (messages.length - 1) && messages[i].sender._id !== userId)
     )
     return 0;
+    
     else return "auto";
 };
 
@@ -24,7 +23,7 @@ const isSameUser = (messages, m, i) => {
 
 const isSameSender = (allMessages, currentMessage, index, userId) => {
     return (
-        index < allMessages.length - 1 && 
+        index < (allMessages.length - 1) && 
         (allMessages[index+1].sender._id !== currentMessage.sender._id ||
         allMessages[index+1].sender._id === undefined) && 
         allMessages[index].sender._id !== userId
@@ -33,7 +32,7 @@ const isSameSender = (allMessages, currentMessage, index, userId) => {
 
 const isLastMessage = (allMessages, index, userId) => {
     return (
-        index === allMessages.length - 1 &&
+        index === (allMessages.length - 1) &&
         allMessages[allMessages.length - 1].sender._id !== userId &&
         allMessages[allMessages.length - 1].sender._id
     )
@@ -44,32 +43,37 @@ const ScrollableChat = ({ messages }) => {
     
     return (
         <div className="scrollableField">
+            
             {messages && messages.map((mes, index) => {
                 return (
-                <div>
-                    <div style={{ display: "flex" }} key={mes._id}>
+                <div style={{ display: "flex", alignItems: "center" }} >
+                    
+                    <div key={mes._id}>
                         {(isSameSender(messages, mes, index, user.user_Id)
                         || isLastMessage(messages, index, user.user_Id)
                         ) && (
-                            <div label={mes.sender.name} placement="bottom-start">
-                                <img src={mes.sender.picture} alt="Avatar" className="avatar" />
-                            </div>
+                        <div label={mes.sender.name} placement="bottom-start">
+                            <img src={mes.sender.picture} alt="Avatar" className="avatar" />
+                        </div>
                         )}
                     </div>
-
+                    
                     <span className="messageContent" style={{
+                        display: "table",
                         backgroundColor: `${mes.sender._id === user.user_Id ? "#BEE3F8" : "#B9F5D0"}`,
                         marginLeft: isSameSenderMargin(messages, mes, index, user.user_Id),
                         marginTop: isSameUser(messages, mes, index, user.user_Id) ? 3 : 10,
-                        borderRadius: "18px",
+                        borderRadius: "16px",
                         padding: "5px 15px",
                         maxWidth: "75%"
                     }}>
                         {mes.content}
                     </span>
+
                 </div>
                 )
             })}
+
         </div>
     )
 }
