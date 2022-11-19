dcde2510661b698dffc6858cb15cd130418c7d86
Kirill Salfetnik <oriole001@gmail.com>
2022-11-19 17:26:16 +0100

    Continue deploying

--- frontend/src/components/ChatWindow.js
+++ frontend/src/components/ChatWindow.js
@@ -5,7 +5,7 @@ import ScrollableChat from './ScrollableChat';
 import io from 'socket.io-client';
 import $ from 'jquery';
 
-const ENDPOINT = "http://localhost:4000";
+const ENDPOINT = "https://chat-socialize.herokuapp.com/";
 var socket, selectedChatCompare;
 
 const ChatWindow = () => {
