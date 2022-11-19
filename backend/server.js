527f6578a2ad3cbce85bebc1c9936cfed357e2af
Kirill Salfetnik <oriole001@gmail.com>
2022-11-19 17:09:29 +0100

    Start deployment

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,11 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const path = require('path');
 const userRoutes = require('./routes/userRoutes');
 const chatRoutes = require('./routes/chatRoutes');
 const messageRoutes = require('./routes/messageRoutes');
+const e = require('express');
 
 // express app
 const app = express();
@@ -23,6 +25,26 @@ app.use((req, res, next) => {
 app.use('/api/user', userRoutes);
 app.use('/api/chat', chatRoutes);
 app.use('/api/message', messageRoutes);
+
+
+//*********************** Deployment *******************
+
+const __dirname1 = path.resolve();
+if(process.env.NODE_ENV === 'production') {
+
+    app.use(express.static(path.join(__dirname1, "/frontend/build")));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"));
+    })
+} else {
+    app.get("/", (req, res) => {
+        res.send("API is running successfully");
+    })
+}
+
+//*********************** Deployment *******************
+
 
 // connect to database
 mongoose.connect(process.env.MONGO_URI)
