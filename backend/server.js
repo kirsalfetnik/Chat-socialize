b0ad2c1730fa7ca88049885ceeaebe582e94d252
Kirill Salfetnik <oriole001@gmail.com>
2022-11-19 20:33:05 +0100

    Still deploying

--- backend/server.js
+++ backend/server.js
@@ -35,7 +35,10 @@ if(process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname1, "/frontend/build")));
 
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"));
+        // res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"));
+
+        const index = path.join(__dirname1, 'frontend', 'build', 'index.html');
+        res.sendFile(index);
     })
 } else {
     app.get("/", (req, res) => {
